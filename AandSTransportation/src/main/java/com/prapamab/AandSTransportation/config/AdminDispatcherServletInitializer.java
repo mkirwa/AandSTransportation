d8a6db30c48fccca9b6f976d881fa76900b27038
mkirwa <mahlonkibiwott@gmail.com>
2018-12-29 18:50:59 -0700

    Add files via upload

---
+++ AandSTransportation/src/main/java/com/prapamab/AandSTransportation/config/AdminDispatcherServletInitializer.java
@@ -0,0 +1,55 @@
+package com.prapamab.AandSTransportation.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+//the front initializer will be initialized by this particular class
+
+public class AdminDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer
+{
+
+		@Override
+		//here, you have to return the root configuration
+		//there could be multiple root configurations
+		//it's for this reason that it is returning class
+		//array. ---Class<?>[] ----
+		protected Class<?>[] getRootConfigClasses() {
+			// TODO Auto-generated method stub
+			//this is actually the business layer
+			return new Class[] {SpringRootConfig.class};
+		}
+		
+		@Override
+		//this is web specific beans
+		protected Class<?>[] getServletConfigClasses() {
+			// TODO Auto-generated method stub
+			return new Class[] {SpringWebConfig.class};
+
+		}
+		
+		@Override
+		//dispatcher servlet is a front controller
+		//it will receive a specific pattern of the url
+		//there can be multiple patterns here since
+		//servlets can allow multiple mappings
+		//hence the front controller will receive all 
+		//the requests for example /admin/blah....
+		protected String[] getServletMappings() {
+			// TODO Auto-generated method stub
+			return new String[]{"/"};
+		}
+		
+		//You can add this method here if required in future
+		//sometimes when you write a complex web application or
+		//you are upgrading your old application to the new application
+		//don't ignore this....must be present. If this is missed, 
+		//then the application might not work
+		//ServletContext is the object for the whole application
+		@Override
+		public void onStartup(ServletContext servletContext) throws ServletException{
+			super.onStartup(servletContext);//don't miss this line....if you do the framework won't start
+			//configure global objects/tasks here if required...
+		}
+}
--- AandSTransportation/src/main/java/com/prapamab/AandSTransportation/config/SpringWebConfig.java
+++ AandSTransportation/src/main/java/com/prapamab/AandSTransportation/config/SpringWebConfig.java
@@ -1,5 +1,60 @@
 package com.prapamab.AandSTransportation.config;
 
-public class SpringWebConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@ComponentScan(basePackages={"com.prapamab"})
+//enabling web mvc will activate your configuration module
+//very important
+@EnableWebMvc
+//if you require you can add one resource here....the import resource
+//You also have to extend webmvc configurer adapter
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
+	
+	//this method handles static resources...
+	//spring won't allow any resources to be accessed on the JSP page
+	//if it's not configured!
+	//so to access static resources like style CSS files
+	//JavaScript files or some other static resources
+	//they must be added here
+	public void addResourceHandlers(ResourceHandlerRegistry registry){
+		//TODO: add the static resources here
+		
+	}
+	//since it's spring mvc, three major components will be present in this
+	//layer
+	//1. Controller spring can search the controller from com.prapamab
+	//	 package through the annotation scanning
+	//2.ViewResolver
+	//3.HandlerMapping
+		
+	//view resolver component...this is a bean actually
+	
+		
+	@Bean
+	public ViewResolver viewResolver() {
+		//internal resource refer to the resources available inside the 
+		//private location.
+		//the private directory is your web information folder. 
+		//because no one can access the contents from the browser directly
+
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+		vr.setViewClass(JstlView.class);
+		//prefix defines the location where JSPs are available
+		vr.setPrefix("WEB-INF/view/");
+		//suffix is the extension of your view
+		vr.setSuffix(".jsp");
+		return vr;
+	}
+		
+	
 
 }
