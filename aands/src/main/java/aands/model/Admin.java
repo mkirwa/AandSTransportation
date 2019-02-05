7aa23bf7d4bd6e8dacc0924f0b8376958bc31687
mkirwa <mahlonkibiwott@gmail.com>
2019-02-04 22:10:00 -0700

    Add files via upload

--- aands/src/main/java/aands/model/Admin.java
+++ aands/src/main/java/aands/model/Admin.java
@@ -2,4 +2,17 @@ package aands.model;
 
 public class Admin {
 
+	private String name;
+	private String address;
+	private String emailAddress;
+	private Integer phoneNumber;
+	private String race;
+	private String countryOfCitizenship; 
+	private Integer drivingLicenseNo;
+	private String emergencyContactName;
+	private Integer emergencyContactNumber;
+	private String username;
+	private String password;
+	private Integer role;
+	private Integer loginStatus;
 }
