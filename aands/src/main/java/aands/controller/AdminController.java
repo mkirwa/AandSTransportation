617d641ff94fd29da21437cecc4cf371fe9d983f
mkirwa <mahlonkibiwott@gmail.com>
2019-02-18 23:01:33 -0600

    Add files via upload

--- aands/src/main/java/aands/controller/AdminController.java
+++ aands/src/main/java/aands/controller/AdminController.java
@@ -1,10 +1,23 @@
 package aands.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import aandsspring.service.AdminService;
 
 @Controller
 @RequestMapping(value="/Admin")
 public class AdminController {
 
+	@Autowired
+	AdminService adminService;
+	
+	@RequestMapping(value="",method= RequestMethod.GET)
+	public ModelAndView list() {
+		return null;
+		
+	}
 }
