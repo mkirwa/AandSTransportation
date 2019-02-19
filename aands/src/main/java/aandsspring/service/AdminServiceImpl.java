889fe0e4d8a81d6731d0b59d2e3e1244dfe43f05
mkirwa <mahlonkibiwott@gmail.com>
2019-02-18 22:23:19 -0600

    Add files via upload

--- aands/src/main/java/aandsspring/service/AdminServiceImpl.java
+++ aands/src/main/java/aandsspring/service/AdminServiceImpl.java
@@ -2,8 +2,10 @@ package aandsspring.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import aands.dao.AdminDao;
 import aands.model.Admin;
 //used to mark a class as a service provider
 //used with classes that provide some functionalites
@@ -11,6 +13,19 @@ import aands.model.Admin;
 //can be applied only to classes
 @Service
 public class AdminServiceImpl implements AdminService{
+
+	
+	AdminDao adminDao;
+	
+	// Tells the application context to inject an instance of AdminService here
+	//All spring beans are managed - they live inside a
+	//container called "application context"
+	//each application has an entry to that context. Web applications have a servlet as this entry
+	//
+	@Autowired
+	public void setAdminDao(AdminDao adminDao) {
+		this.adminDao = adminDao;
+	}
 
 	public List<Admin> listAllAdmins() {
 		// TODO Auto-generated method stub
