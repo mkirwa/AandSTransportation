c01724d192c018fe440e555a6e0960b2cf434a3d
mkirwa <mahlonkibiwott@gmail.com>
2019-02-18 22:55:47 -0600

    Add files via upload

---
+++ aands/src/main/java/aands/controller/AdminController.java
@@ -0,0 +1,10 @@
+package aands.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/Admin")
+public class AdminController {
+
+}
