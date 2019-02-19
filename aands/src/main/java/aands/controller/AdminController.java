acd59f0160e84503b1e10e5503c0077448dc089d
mkirwa <mahlonkibiwott@gmail.com>
2019-02-18 23:11:27 -0600

    Add files via upload

--- aands/src/main/java/aands/controller/AdminController.java
+++ aands/src/main/java/aands/controller/AdminController.java
@@ -7,7 +7,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import aandsspring.service.AdminService;
-
+//when a request is made to a spring application, 
+//the controller is responsible for returning the response
+//to that request.
+//the controller action can return different types of action results 
+//to a particular request
+//the Controller is responsible for controlling the application logic and 
+//acts as the coordinator between the View and the Model. 
+//The Controller receives an input from the users via the View,
+//then processes the user's data with the help of Model and passes the results back to the View.
 @Controller
 @RequestMapping(value="/Admin")
 public class AdminController {
