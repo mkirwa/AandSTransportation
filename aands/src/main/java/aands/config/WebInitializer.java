dbfdea122ab9007dac423e5c18f4ba30b93169f5
mkirwa <mahlonkibiwott@gmail.com>
2019-02-28 22:36:07 -0700

    Add files via upload

--- aands/src/main/java/aands/config/WebInitializer.java
+++ aands/src/main/java/aands/config/WebInitializer.java
@@ -1,5 +1,25 @@
 package aands.config;
 
-public class WebInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
