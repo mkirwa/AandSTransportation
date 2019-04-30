014689cb38857b1a25e96fdee506b95a8f92c0bf
mkirwa <mahlonkibiwott@gmail.com>
2019-04-29 22:01:57 -0600

    Add files via upload

---
+++ src/main/java/com/mahlon/service/UserService.java
@@ -0,0 +1,18 @@
+package com.mahlon.service;
+
+import java.util.List;
+
+import com.mahlon.model.User;
+
+public interface UserService {
+
+	public List listAllUsers();
+	 
+	 public void addUser(User user);
+	 
+	 public void updateUser(User user);
+	 
+	 public void deleteUser(int id);
+	 
+	 public User findUserById(int id);
+}
