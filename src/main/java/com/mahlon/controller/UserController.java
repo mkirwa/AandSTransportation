a40957762563917c0e1ca16a14ed20bf4ac1e1c1
mkirwa <mahlonkibiwott@gmail.com>
2019-05-01 17:05:35 -0600

    Add files via upload

---
+++ src/main/java/com/mahlon/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.mahlon.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.jackrutorial.model.User;
+import com.jackrutorial.service.UserService;
+
+@Controller
+@RequestMapping(value="/user")
+
+public class UserController {
+	
+	
+
+}
