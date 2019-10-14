f68b1d0dc2b81e86da62bdca16e0caeb97f19716
mkirwa <mahlonkibiwott@gmail.com>
2019-10-13 21:27:28 -0600

    Add files via upload

--- src/components/userform/userform.jsx
+++ src/components/userform/userform.jsx
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 class UserForm extends Component {
   state = {
     step: 1,
-    firstName: ""
+    firstName: "",
+    lastName: "",
+    email: ""
   };
   render() {
     return <div>This is the user form</div>;
