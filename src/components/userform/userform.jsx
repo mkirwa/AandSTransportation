8e6ef4d30530524a809722f2c791a640c9ff0d81
mkirwa <mahlonkibiwott@gmail.com>
2019-10-15 19:39:22 -0600

    Add files via upload

--- src/components/userform/userform.jsx
+++ src/components/userform/userform.jsx
@@ -19,6 +19,20 @@ class UserForm extends Component {
     });
   };
 
+  //go back to previous step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  //handle fields change
+  //each input will have their own state which would be the firstName etc
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
+  };
+
   render() {
     return <div>This is the user form</div>;
   }
