444ea69ad37c41907f4eb74a3841df55be9ba93e
mkirwa <mahlonkibiwott@gmail.com>
2019-10-15 19:32:07 -0600

    Add files via upload

--- src/components/userform/userform.jsx
+++ src/components/userform/userform.jsx
@@ -5,8 +5,20 @@ class UserForm extends Component {
     step: 1,
     firstName: "",
     lastName: "",
-    email: ""
+    email: "",
+    occupation: "",
+    city: "",
+    bio: ""
   };
+
+  //has a method to proceed to the next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+
   render() {
     return <div>This is the user form</div>;
   }
