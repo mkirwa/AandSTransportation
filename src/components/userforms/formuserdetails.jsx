ffefb55a29545e2844bdc2627a597856cefa71d3
mkirwa <mahlonkibiwott@gmail.com>
2019-10-16 20:10:32 -0600

    Add files via upload

--- src/components/userforms/formuserdetails.jsx
+++ src/components/userforms/formuserdetails.jsx
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
 import TextField from "material-ui/TextField";
 import RaisedButton from "material-ui/RaisedButton";
 
 class FormUserDetails extends Component {
-  state = {};
+  continue = e => {
+    //prevent default
+    e.preventDefault();
+    this.props.nextStep();
+  };
+
   render() {
-    return <div>This is the user form details</div>;
+    //pulling values from the props in continue
+    //doing this so you can use values as a variable
+    const { values } = this.props;
+    return (
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title="Enter User Details" />
+          <TextField
+            hintText="Enter Your First Name"
+            floatingLabelText="First Name"
+          />
+        </React.Fragment>
+      </MuiThemeProvider>
+    );
   }
 }
 
