4b7557c202aea3441c7122d767495634a7f4eb12
mkirwa <mahlonkibiwott@gmail.com>
2019-10-18 19:43:47 -0600

    Add files via upload

--- src/components/userforms/usersform.jsx
+++ src/components/userforms/usersform.jsx
@@ -4,27 +4,29 @@ import FormControl from "react-bootstrap/lib/FormControl";
 class UsersForm extends Component {
   render() {
     return (
-      <div>
-        <br />
-        <form className>
-          <div className="form-row">
-            <div className="col">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="First name"
-              />
+      <main>
+        <div>
+          <br />
+          <form className>
+            <div className="form-row">
+              <div className="col-md-5 mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="First name"
+                />
+              </div>
+              <div className="col-md-5 mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Last name"
+                />
+              </div>
             </div>
-            <div className="col">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Last name"
-              />
-            </div>
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
+      </main>
     );
   }
 }
