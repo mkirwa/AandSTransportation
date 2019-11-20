08fed51c2db0ec7257e825061aaeeda93005ab02
mkirwa <mahlonkibiwott@gmail.com>
2019-11-19 21:06:34 -0700

    Add files via upload

---
+++ src/components/login/renderer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { ReactDOM } from "react-dom";
+
+class Apps extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+}
