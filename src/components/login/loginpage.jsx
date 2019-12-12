0360b4a74d489eabc58227f5fa10a0d406174a2e
mkirwa <mahlonkibiwott@gmail.com>
2019-12-11 22:58:05 -0700

    Add files via upload

--- src/components/login/loginpage.jsx
+++ src/components/login/loginpage.jsx
@@ -1,43 +1,23 @@
 import React, { Component } from "react";
+import LoginBox from './loginbox';
 
 class LoginPage extends Component {
   state = {};
   render() {
     return (
-      <div className="inner-container">
-        <div className="header">Login</div>
-        <div className="box">
-          <div className="input-group">
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              name="username"
-              className="login-input"
-              placeholder="Username"
-            />
-          </div>
-
-          <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              className="login-input"
-              placeholder="Password"
-            />
-          </div>
-
-          <button
-            type="button"
-            className="login-btn"
-            //onClick={this.submitLogin.bind(this)}
-          >
-            Login
-          </button>
+      <div className="root-container">
+        <div className="box-controller">
+          <div className="controller">Login</div>
+          <div className="controller">Register</div>
         </div>
+        <div className="box-container"></div>
       </div>
     );
   }
+}
+
+class LoginBox extends from React.Component{
+  
 }
 
 export default LoginPage;
