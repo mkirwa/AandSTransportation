0e265d52e626904d965fcd34a7418ab9ecfc4523
mkirwa <mahlonkibiwott@gmail.com>
2019-12-11 23:10:45 -0700

    Add files via upload

--- src/components/login/loginpage.jsx
+++ src/components/login/loginpage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import LoginBox from './loginbox';
+//import LoginBox from "./loginbox";
 
 class LoginPage extends Component {
   state = {};
@@ -16,20 +16,16 @@ class LoginPage extends Component {
   }
 }
 
-class LoginBox extends from React.Component{
-
-  constructor(props){
+class LoginBox extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={};
+    this.state = {};
   }
-  render(){
+  render() {
     <div className="inner-container">
-      <div className="box">
-
-      </div>
-    </div>
+      <div className="box"></div>
+    </div>;
   }
-
 }
 
 export default LoginPage;
