d1cb61b72e250bda7525147149f3150ac5e28018
mkirwa <mahlonkibiwott@gmail.com>
2020-03-18 20:18:20 -0600

    Add files via upload

--- src/components/dispatchs/dispatch.jsx
+++ src/components/dispatchs/dispatch.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import DispatchNavbar from "./dispatchnavbar";
-import Users from "../users";
+import UserForm from "../userforms/userform";
 
 class Dispatch extends Component {
   render() {
     return (
       <div>
         <DispatchNavbar />
-        <Users />
+        <UserForm />
       </div>
     );
   }
