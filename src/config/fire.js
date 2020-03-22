bd984ed25988e53bf92e78ebd9c08561de96be87
mkirwa <mahlonkibiwott@gmail.com>
2020-03-21 19:28:41 -0600

    Add files via upload

---
+++ src/config/fire.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEDPIb7XJ9cJyFnXhEUzp2RW1L4t0zCoY",
+  authDomain: "jipe-app.firebaseapp.com",
+  databaseURL: "https://jipe-app.firebaseio.com",
+  projectId: "jipe-app",
+  storageBucket: "jipe-app.appspot.com",
+  messagingSenderId: "954348692389",
+  appId: "1:954348692389:web:304dfa07e15a4a04097e4c",
+  measurementId: "G-EQEBKD045N"
+};
