a0ded8553143f9f98c74c59e3b2c9239c099404f
mkirwa <mahlonkibiwott@gmail.com>
2020-06-11 21:29:01 -0600

    Add files via upload

--- src/components/driver/navbardriver.jsx
+++ src/components/driver/navbardriver.jsx
@@ -91,7 +91,7 @@ class NavBarDriver extends Component {
             </li>
           </ul>
         </div>
-        <button type="button" onClick={this.submitLogin.bind(this)}>
+        <button type="button" onClick={this.submitLogout.bind(this)}>
           Sign Out
         </button>
       </nav>
