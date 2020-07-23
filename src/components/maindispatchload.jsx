bfac2daf68140264382d6850b7690681513f840e
mkirwa <mahlonkibiwott@gmail.com>
2020-07-22 22:34:37 -0600

    Add files via upload

--- src/components/maindispatchload.jsx
+++ src/components/maindispatchload.jsx
@@ -12,10 +12,10 @@ class MainDispatchLoad extends Component {
         <div style={styles}>
           <h5>Pick Up Address</h5>
         </div>
-        <React.Fragment>
-          <div style={style}>
+        <div className="box-container">
+          <React.Fragment>
             <TextField
-              placholder="Company name"
+              placeholder="Company name"
               label="Company Name"
               //How react works, everytime the textfield changes
               //everytime we type in it, it will fire off an event
@@ -24,183 +24,199 @@ class MainDispatchLoad extends Component {
               //onChange={this.handleChange.bind(this)("Company Name")}
               //defaultValue={values.companyName}
               margin="normal"
+              fullWidth
             />
-          </div>
-          <div>
-            <h4></h4>
-          </div>
 
-          <div style={style}>
             <TextField
-              hintText="Enter Company Street Address"
-              floatingLabelText="Street Address"
+              placeholder="Enter Pick Up Street Address"
+              label="Street Address"
               //How react works, everytime the textfield changes
               //everytime we type in it, it will fire off an event
               //of onChange.....You will have firstName in brackets coz
               //handleChange takes in an input
               //onChange={this.handleChange("bio")}
               //defaultValue={values.bio}
+              margin="normal"
+              fullWidth
             />
-          </div>
 
-          <div style={style}>
             <TextField
-              hintText="Enter Suite Number"
-              floatingLabelText="Suite Number"
+              placeholder="Enter Suite Number"
+              label="Suite Number"
               //How react works, everytime the textfield changes
               //everytime we type in it, it will fire off an event
               //of onChange.....You will have firstName in brackets coz
               //handleChange takes in an input
               //onChange={this.handleChange("bio")}
               //defaultValue={values.bio}
+              margin="normal"
+              fullWidth
             />
-          </div>
 
-          <div style={style}>
             <TextField
-              hintText="Enter Your City"
-              floatingLabelText="City"
+              placeholder="Enter Pick Up City"
+              label="City"
               //How react works, everytime the textfield changes
               //everytime we type in it, it will fire off an event
               //of onChange.....You will have firstName in brackets coz
               //handleChange takes in an input
               //onChange={this.handleChange("bio")}
               //defaultValue={values.bio}
+              margin="normal"
+              fullWidth
             />
-          </div>
 
-          <div style={style}>
             <TextField
-              hintText="Enter Your State"
-              floatingLabelText="State"
+              placeholder="Enter Pick Up State"
+              label="State"
               //How react works, everytime the textfield changes
               //everytime we type in it, it will fire off an event
               //of onChange.....You will have firstName in brackets coz
               //handleChange takes in an input
               //onChange={this.handleChange("bio")}
               //defaultValue={values.bio}
+              margin="normal"
+              fullWidth
             />
-          </div>
 
-          <div style={style}>
             <TextField
-              hintText="Zip Code"
-              floatingLabelText="Zip Code"
+              placeholder="Zip Code"
+              label="Zip Code"
               //How react works, everytime the textfield changes
               //everytime we type in it, it will fire off an event
               //of onChange.....You will have firstName in brackets coz
               //handleChange takes in an input
               //onChange={this.handleChange("bio")}
               //defaultValue={values.bio}
+              margin="normal"
+              fullWidth
             />
-          </div>
-          <div style={styles}>
-            <h5>Drop of Address</h5>
-          </div>
-          <div style={style}>
-            <TextField
-              hintText="Company name"
-              floatingLabelText="Company Name"
-              //How react works, everytime the textfield changes
-              //everytime we type in it, it will fire off an event
-              //of onChange.....You will have firstName in brackets coz
-              //handleChange takes in an input
-              //onChange={handleChange("occupation")}
-              //defaultValue={values.occupation}
-            />
-          </div>
-          <div>
-            <h4></h4>
-          </div>
 
-          <div style={style}>
-            <TextField
-              hintText="Enter Company Street Address"
-              floatingLabelText="Street Address"
-              //How react works, everytime the textfield changes
-              //everytime we type in it, it will fire off an event
-              //of onChange.....You will have firstName in brackets coz
-              //handleChange takes in an input
-              //onChange={this.handleChange("bio")}
-              //defaultValue={values.bio}
-            />
-          </div>
+            <div style={styles}>
+              <h5>Drop of Address</h5>
+            </div>
+            <div style={style}>
+              <TextField
+                placeholder="Company name"
+                label="Company Name"
+                //How react works, everytime the textfield changes
+                //everytime we type in it, it will fire off an event
+                //of onChange.....You will have firstName in brackets coz
+                //handleChange takes in an input
+                //onChange={handleChange("occupation")}
+                //defaultValue={values.occupation}
+                margin="normal"
+                fullWidth
+              />
+            </div>
+            <div>
+              <h4></h4>
+            </div>
 
-          <div style={style}>
-            <TextField
-              hintText="Enter Suite Number"
-              floatingLabelText="Suite Number"
-              //How react works, everytime the textfield changes
-              //everytime we type in it, it will fire off an event
-              //of onChange.....You will have firstName in brackets coz
-              //handleChange takes in an input
-              //onChange={this.handleChange("bio")}
-              //defaultValue={values.bio}
-            />
-          </div>
+            <div style={style}>
+              <TextField
+                placeholder="Enter Company Drop Off Street Address"
+                label="Street Address"
+                //How react works, everytime the textfield changes
+                //everytime we type in it, it will fire off an event
+                //of onChange.....You will have firstName in brackets coz
+                //handleChange takes in an input
+                //onChange={this.handleChange("bio")}
+                //defaultValue={values.bio}
+                margin="normal"
+                fullWidth
+              />
+            </div>
 
-          <div style={style}>
-            <TextField
-              hintText="Enter Your City"
-              floatingLabelText="City"
-              //How react works, everytime the textfield changes
-              //everytime we type in it, it will fire off an event
-              //of onChange.....You will have firstName in brackets coz
-              //handleChange takes in an input
-              //onChange={this.handleChange("bio")}
-              //defaultValue={values.bio}
-            />
-          </div>
+            <div style={style}>
+              <TextField
+                placeholder="Enter Drop Off Suite Number"
+                label="Suite Number"
+                //How react works, everytime the textfield changes
+                //everytime we type in it, it will fire off an event
+                //of onChange.....You will have firstName in brackets coz
+                //handleChange takes in an input
+                //onChange={this.handleChange("bio")}
+                //defaultValue={values.bio}
+                margin="normal"
+                fullWidth
+              />
+            </div>
 
-          <div style={style}>
-            <TextField
-              hintText="Enter Your State"
-              floatingLabelText="State"
-              //How react works, everytime the textfield changes
-              //everytime we type in it, it will fire off an event
-              //of onChange.....You will have firstName in brackets coz
-              //handleChange takes in an input
-              //onChange={this.handleChange("bio")}
-              //defaultValue={values.bio}
-            />
-          </div>
+            <div style={style}>
+              <TextField
+                placeholder="Enter Drop Off City"
+                label="City"
+                //How react works, everytime the textfield changes
+                //everytime we type in it, it will fire off an event
+                //of onChange.....You will have firstName in brackets coz
+                //handleChange takes in an input
+                //onChange={this.handleChange("bio")}
+                //defaultValue={values.bio}
+                margin="normal"
+                fullWidth
+              />
+            </div>
 
-          <div style={style}>
-            <TextField
-              hintText="Zip Code"
-              floatingLabelText="Zip Code"
-              //How react works, everytime the textfield changes
-              //everytime we type in it, it will fire off an event
-              //of onChange.....You will have firstName in brackets coz
-              //handleChange takes in an input
-              //onChange={this.handleChange("bio")}
-              //defaultValue={values.bio}
-            />
-          </div>
+            <div style={style}>
+              <TextField
+                placeholder="Enter Drop Off State"
+                label="State"
+                //How react works, everytime the textfield changes
+                //everytime we type in it, it will fire off an event
+                //of onChange.....You will have firstName in brackets coz
+                //handleChange takes in an input
+                //onChange={this.handleChange("bio")}
+                //defaultValue={values.bio}
+                margin="normal"
+                fullWidth
+              />
+            </div>
 
-          <div style={styles}>
+            <div style={style}>
+              <TextField
+                placeholder="Zip Code"
+                label="Zip Code"
+                //How react works, everytime the textfield changes
+                //everytime we type in it, it will fire off an event
+                //of onChange.....You will have firstName in brackets coz
+                //handleChange takes in an input
+                //onChange={this.handleChange("bio")}
+                //defaultValue={values.bio}
+                margin="normal"
+                fullWidth
+              />
+            </div>
+
+            <div style={styles}>
+              <Button
+                //we put contiue up above and what contiunue does
+                //is call the next step of userForm and we do that through
+                color="primary"
+                variant="contained"
+                style={styles.button}
+                //we put contiue up above and what contiunue does
+                //is call the next step of userForm and we do that through
+                //props
+                onClick={this.continue}
+              >
+                Continue
+              </Button>
+            </div>
+
             <Button
-              label="Continue"
-              primary={true}
+              color="secondary"
+              variant="contained"
               style={styles.button}
               //we put contiue up above and what contiunue does
               //is call the next step of userForm and we do that through
               //props
-              onClick={this.continue}
-            />
-          </div>
-
-          <Button
-            label="Back"
-            //makes the button a white
-            primary={false}
-            style={styles.button}
-            //we put contiue up above and what contiunue does
-            //is call the next step of userForm and we do that through
-            //props
-            onClick={this.back}
-          />
-        </React.Fragment>
+              onClick={this.back}
+            >
+              Back
+            </Button>
+          </React.Fragment>
+        </div>
       </MuiThemeProvider>
     );
   }
